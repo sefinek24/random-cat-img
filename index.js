5d2ad3e7e63f330d7b3789bfb81a92765313f2e8
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-15 08:21:50 +0200

    Updated headers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { version } = require('./package.json');
 
 async function getCat() {
 	return await get('https://api.skiffybot.xyz/api/v1/animals/cat', {
-		headers: { 'User-Agent': `random-cat-img (https://github.com/sefinek24/random-cat-img) / v${version}` },
+		headers: { 'User-Agent': `random-cat-img/${version} (https://github.com/sefinek24/random-cat-img)` },
 	});
 }
 
