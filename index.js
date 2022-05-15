c9cc5954afac66b515fd24c68b3cfc6eabc5084c
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-15 08:23:46 +0200

    Update

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const { get } = require('axios');
 const { version } = require('./package.json');
 
 module.exports = async () => {
-	return await get('https://api.skiffybot.xyz/api/v1/animals/cat', {
-		headers: { 'User-Agent': `random-cat-img/${version} (https://github.com/sefinek24/random-cat-img)` },
-	});
+	return await get('https://api.skiffybot.xyz/api/v1/animals/cat', { headers: { 'User-Agent': `random-cat-img/${version} (https://github.com/sefinek24/random-cat-img)` } });
 };
