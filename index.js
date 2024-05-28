9a6169ca7372cc44b2d0eeac45146f37273b7dda
Sefinek <contact@sefinek.net>
2024-05-28 17:24:46 +0200

    Cleanup

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const headers = {
 	'Referrer-Policy': 'strict-origin-when-cross-origin',
 	'X-Content-Type-Options': 'nosniff',
 	'X-Frame-Options': 'DENY',
-	'X-XSS-Protection': '1; mode=block',
+	'X-XSS-Protection': '1; mode=block'
 };
 
 /**
